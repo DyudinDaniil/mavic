c9c9dc7072eb318d6dbfd2d6b3d1729db2c753cc
DyudinDaniil <DyudinDaniil2002@gmail.com>
2021-01-02 22:49:11 +0300

    end of desctop

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,9 @@ $(function(){
         nextArrow: '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
         infinite: false,
     });
+
+    $('.questions__items-item-title').on('click', function(){
+        $('.questions__items-item').removeClass('questions__items-item_active');
+        $(this).parent().addClass('questions__items-item_active');
+    });
 });
